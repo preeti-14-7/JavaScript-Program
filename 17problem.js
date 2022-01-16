ef49903e4f92b6540c5bd334696b51f2970daf0e
Abhay Anand <48680107+Abhay0809@users.noreply.github.com>
2022-01-17 00:58:49 +0530

    Create 17problem.js

---
+++ 17problem.js
@@ -0,0 +1,14 @@
+function binarySearch(arr,value,startPos,endPos){
+  if(startPos > endPos) return -1;
+
+  let middleIndex = Math.floor(startPos+endPos)/2;
+
+  if(arr[middleIndex] === value) return middleIndex;
+
+  elsif(arr[middleIndex > value]){
+    return binarySearch(arr,value,startPos,middleIndex-1);
+  }
+  else{
+    return binarySearch(arr,value,middleIndex+1,endPos);
+  }
+}   
