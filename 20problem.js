fcaa3094c3351ede6feba4eae9738c0cc2b4f9a6
Abhay Anand <48680107+Abhay0809@users.noreply.github.com>
2022-01-25 23:19:04 +0530

    Rotation problem

---
+++ 20problem.js
@@ -0,0 +1,13 @@
+function rotateRight(arr,rotations){
+  if(rotations == 0) return arr;
+  for(let i = 0; i < rotations;i++){
+    let element = arr.pop();
+    arr.unshift(element);
+  }
+
+  return arr;
+}
+
+
+rotateRight([2, 3, 4, 5, 7], 3); // Return [4,5,7,2,3]
+rotateRight([44, 1, 22, 111], 5); // Returns [111,44,1,22]   
